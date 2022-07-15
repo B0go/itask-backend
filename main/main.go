12063bfd1692fbde7b902ba35b6c3f9670d50711
B0go <victorbogo@icloud.com>
2022-07-14 23:14:10 -0300

    initial work using gorm and gin
    Signed-off-by: B0go <victorbogo@icloud.com>

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/B0go/itask-backend/model"
@@ -36,7 +37,10 @@ func main() {
 			return
 		}
 
-		db.Create(&task)
+		if err := db.Create(&task).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
 
 		c.JSON(http.StatusCreated, task)
 	})
@@ -48,9 +52,37 @@ func main() {
 			return
 		}
 
-		db.Create(&tasks)
+		if err := db.Create(&tasks).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
 
 		c.JSON(http.StatusCreated, tasks)
+	})
+
+	r.DELETE("/tasks/:uuid", func(c *gin.Context) {
+		var task model.Task
+		task.Uuid = c.Param("uuid")
+
+		if err := db.Delete(&task).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		c.Status(http.StatusOK)
+	})
+
+	r.GET("/tasks/:uuid", func(c *gin.Context) {
+		var task model.Task
+		if err := db.First(&task, "uuid = ?", c.Param("uuid")).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.Status(http.StatusNotFound)
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			}
+			return
+		}
+		c.JSON(http.StatusOK, &task)
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
--- model/task.go
+++ model/task.go
@@ -4,10 +4,10 @@ import "time"
 
 //Task is the object that holds task data
 type Task struct {
-	Uuid        string    `json:"uuid"`
-	Title       string    `json:"title"`
-	Creation    time.Time `json:"creation"`
-	DueDate     time.Time `json:"dueDate"`
-	Description string    `json:"description"`
-	Completed   bool      `json:"completed"`
+	Uuid        string    `json:"uuid" pg:"uuid" gorm:"primaryKey"`
+	Title       string    `json:"title" pg:"title"`
+	Creation    time.Time `json:"creation" pg:"creation"`
+	DueDate     time.Time `json:"dueDate" pg:"due_date"`
+	Description string    `json:"description" pg:"description"`
+	Completed   bool      `json:"completed" pg:"completed"`
 }
