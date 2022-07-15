d13a2be4d1c291afcb0397e7db5b666f05ccf872
B0go <victorbogo@icloud.com>
2022-07-14 22:22:39 -0300

    start building the API
    Signed-off-by: B0go <victorbogo@icloud.com>

--- main/main.go
+++ main/main.go
@@ -1,28 +1,56 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/B0go/itask-backend/model"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
-	fmt.Printf("something")
+	dsn := "user=itask_app dbname=itask sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&model.Task{})
 
 	r := gin.Default()
 
 	r.PUT("/tasks/:uuid", func(c *gin.Context) {
+		u, err := uuid.Parse(c.Param("uuid"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error parsing uuid": err.Error()})
+			return
+		}
 		var task model.Task
+		task.Uuid = u.String()
 		if err := c.ShouldBindJSON(&task); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		task.Uuid = c.Param("uuid")
+		db.Create(&task)
 
-		c.JSON(http.StatusOK, task)
+		c.JSON(http.StatusCreated, task)
+	})
+
+	r.POST("/tasks", func(c *gin.Context) {
+		var tasks []model.Task
+		if err := c.ShouldBindJSON(&tasks); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		db.Create(&tasks)
+
+		c.JSON(http.StatusCreated, tasks)
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
