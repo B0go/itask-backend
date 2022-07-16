12bf018602b97f462dbe45d6fd679e61902b9527
B0go <victorbogo@icloud.com>
2022-07-15 23:04:41 -0300

    use controllers to abstract presentation layer
    Signed-off-by: B0go <victorbogo@icloud.com>

--- main/main.go
+++ main/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/B0go/itask-backend/config"
+	"github.com/B0go/itask-backend/controller"
 	"github.com/B0go/itask-backend/main/db"
-	"github.com/B0go/itask-backend/model"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	_ "github.com/lib/pq"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -21,69 +18,13 @@ func main() {
 	db.MustConnect()
 	db.MustMigrate()
 
+	tc := controller.TaskController{Db: &db}
+
 	r := gin.Default()
-
-	r.PUT("/tasks/:uuid", func(c *gin.Context) {
-		u, err := uuid.Parse(c.Param("uuid"))
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error parsing uuid": err.Error()})
-			return
-		}
-		var task model.Task
-		task.Uuid = u.String()
-		if err := c.ShouldBindJSON(&task); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		if err := db.Db.Create(&task).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err})
-			return
-		}
-
-		c.JSON(http.StatusCreated, task)
-	})
-
-	r.POST("/tasks", func(c *gin.Context) {
-		var tasks []model.Task
-		if err := c.ShouldBindJSON(&tasks); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		if err := db.Db.Create(&tasks).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err})
-			return
-		}
-
-		c.JSON(http.StatusCreated, tasks)
-	})
-
-	r.DELETE("/tasks/:uuid", func(c *gin.Context) {
-		var task model.Task
-		task.Uuid = c.Param("uuid")
-
-		if err := db.Db.Delete(&task).Error; err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err})
-			return
-		}
-
-		c.Status(http.StatusOK)
-	})
-
-	r.GET("/tasks/:uuid", func(c *gin.Context) {
-		var task model.Task
-		if err := db.Db.First(&task, "uuid = ?", c.Param("uuid")).Error; err != nil {
-			if errors.Is(err, gorm.ErrRecordNotFound) {
-				c.Status(http.StatusNotFound)
-			} else {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err})
-			}
-			return
-		}
-		c.JSON(http.StatusOK, &task)
-	})
-
+	r.PUT("/tasks/:uuid", tc.PutTask)
+	r.POST("/tasks", tc.PostTasks)
+	r.DELETE("/tasks/:uuid", tc.DeleteTask)
+	r.GET("/tasks/:uuid", tc.GetTask)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
